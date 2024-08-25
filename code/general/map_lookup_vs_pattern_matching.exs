a0db8d3eb121df17d60e0fa0ce25177c642bbd67
Anderson Cook <andersonmcook@gmail.com>
2024-08-25 08:47:05 -0500

    Backing up

--- code/general/map_lookup_vs_pattern_matching.exs
+++ code/general/map_lookup_vs_pattern_matching.exs
@@ -1,10 +1,10 @@
 defmodule Lookup.Slow do
   @lookup %{
-    "one"   => 1,
-    "two"   => 2,
+    "one" => 1,
+    "two" => 2,
     "three" => 3,
-    "four"  => 4,
-    "five"  => 5
+    "four" => 4,
+    "five" => 5
   }
 
   def int_for(str) do
@@ -17,19 +17,56 @@ defmodule Lookup.Fast do
     do_int_for(str)
   end
 
-  def do_int_for("one"),   do: 1
-  def do_int_for("two"),   do: 2
+  def do_int_for("one"), do: 1
+  def do_int_for("two"), do: 2
   def do_int_for("three"), do: 3
-  def do_int_for("four"),  do: 4
-  def do_int_for("five"),  do: 5
+  def do_int_for("four"), do: 4
+  def do_int_for("five"), do: 5
+end
+
+defmodule Lookup.Map do
+  @lookup %{
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "four" => 4,
+    "five" => 5
+  }
+
+  def int_for(str) do
+    Map.get(@lookup, str)
+  end
+end
+
+defmodule Lookup.CasePattern do
+  @lookup %{
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "four" => 4,
+    "five" => 5
+  }
+
+  def int_for(str) do
+    case @lookup do
+      %{^str => value} -> value
+      _ -> nil
+    end
+  end
 end
 
 defmodule Lookup.Benchmark do
   def benchmark do
-    Benchee.run(%{
-      "Pattern Matching" => fn -> bench_func(Lookup.Fast) end,
-      "Map Lookup"       => fn -> bench_func(Lookup.Slow) end
-    }, time: 10, print: [fast_warning: false])
+    Benchee.run(
+      %{
+        "Function Pattern Matching" => fn -> bench_func(Lookup.Fast) end,
+        "Access Lookup" => fn -> bench_func(Lookup.Slow) end,
+        "Map.get" => fn -> bench_func(Lookup.Map) end,
+        "Case Pattern Matching" => fn -> bench_func(Lookup.CasePattern) end
+      },
+      time: 10,
+      print: [fast_warning: false]
+    )
   end
 
   def bench_func(module) do
